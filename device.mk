13d7d6599044f310f5e98c77d0b8789e864ca5aa
beanstown106 <nbenis106@gmail.com>
2015-10-11 17:34:28 -0400

    Add missing persist.data.qmi.adb_logmask=0 prop
    Change-Id: I1953f64019f8cc43f4b53d5c260670ca60a95544

--- device.mk
+++ device.mk
@@ -222,7 +222,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.apm_sim_not_pwdn=1 \
     persist.radio.no_wait_for_card=1 \
     persist.radio.data_no_toggle=1 \
-    persist.radio.sib16_support=1
+    persist.radio.sib16_support=1 \
+    persist.data.qmi.adb_logmask=0
 
 #Reduce IMS logging
 PRODUCT_PROPERTY_OVERRIDES += \
